ebc14d03f9c86743ee715d372732f9c605ad221b
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-03 20:12:00 +0100

    Exercise 3 comlete
    In 03-*/reveerseString.js there are two methods both are tested and correct

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let charArray = stringToReverse.split('');
+    let char = '';
+    for(let i = 0 ; i < charArray.length / 2 ; i++){
+        char = charArray[i];
+        charArray[i] = charArray[charArray.length-i-1];
+        charArray[charArray.length-i-1] = char;
+    }
+    return charArray.join('');
 };
 
+//second solution
+const reverseString2 = function(stringToReverse){
+    let arrayToReverse = stringToReverse.split('');
+    let reversedString = '';
+    while( arrayToReverse.length != 0 ){
+        reversedString += arrayToReverse.pop();
+    }
+    return reversedString;
+}
 // Do not edit below this line
 module.exports = reverseString;
