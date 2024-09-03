62e0a162744c9eb19994204e54414b385f5d8ddd
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-03 19:11:18 +0100

    Exercise 2 is complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepete, numberOfTimes) {
+    if( numberOfTimes < 0 ){
+        return 'ERROR';
+    }
+    let stringRepeated = '';
+    for( let i = 0; i < numberOfTimes; i++ ){
+        stringRepeated += stringToRepete;
+    }
+    return stringRepeated;
 };
 
 // Do not edit below this line
