c979586b60bf4c9ece7ad1e7c7ff6c7c617c77a0
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-09 14:21:18 +0100

    Seventh exercise is complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFah) {
+  let tempCel = Number( ((tempFah - 32)*5/9).toFixed(1) ) ;
+  return tempCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  let tempFah = Number( ((tempCel * (9/5) )+ 32).toFixed(1) , 2) ;
+  return tempFah;
 };
 
 // Do not edit below this line
