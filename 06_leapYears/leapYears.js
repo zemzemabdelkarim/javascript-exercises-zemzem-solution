d60d996ecb1b1d980f88d37840bfc63e7208d6f3
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-09 14:01:28 +0100

    Sixth exercise comlete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 100 == 0) {
+    year /= 100;
+  }
 
+  if (year % 4 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
