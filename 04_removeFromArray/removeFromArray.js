34e71b700a517690c96d41e94ce554fbf7b2b613
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-09 13:37:50 +0100

    Fourth exercise is complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  console.log(arguments.length);
+
+  for (let j = 1; j < arguments.length; j++) {
+    let value = arguments[j];
+    let valuePosition = array.indexOf(value);
+    if( valuePosition === array.length -1 ){
+        array.pop();
+        continue;
+    }
+    while (valuePosition >= 0) {
+      console.log(`array = ${array}`);
+      console.log(`value = ${value}\nvaluePosition = ${valuePosition}`);
+
+      for (let i = valuePosition; i < array.length - 1; i++) {
+        array[i] = array[i + 1];
+      }
+
+      array.pop();
+      console.log(array);
+      valuePosition = array.indexOf(value);
+    }
+    console.log(array);
+  }
+
+  return array;
 };
 
 // Do not edit below this line
