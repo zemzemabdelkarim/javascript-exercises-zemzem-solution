7313c5688d7d914c8ad56174de8f8af2335b53e2
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-09 13:49:32 +0100

    Fifth exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (...values) {
+    console.log(values.length);
+    
+  if (values.length > 2) {
+    return "ERROR";
+  } else if (!( typeof(values[0]) == 'number') || !( typeof(values[1]) == 'number')) {
+    return "ERROR";
+  } else if (
+    parseInt(values[0]) !== values[0] ||
+    parseInt(values[1]) !== values[1]
+  ) {
+    return "ERROR";
+  }else if ( values[0] < 0 || values[1] < 0 ){
+    return 'ERROR';
+  }
 
+  if( values[0] === values[1] ){
+    //if the same value is provided so the some is 0
+    return 0;
+  }else if( values[0] > values[1] ){
+    //swap the first value and the last value to put the smaller one at the beginning
+    values[0] = values[0] + values[1];
+    values[1] = values[0] - values[1];
+    values[0] = values[0] - values[1];
+  }
+  let sum  = 0;
+  for( let i = values[0]; i < values[1] + 1 ; i++ ){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
