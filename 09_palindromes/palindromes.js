9485625ba91d3012e58348dd30491d3dcec3316b
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-22 16:16:41 +0100

    Exercise 9 is complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,38 @@
-const palindromes = function () {
+const checkChar = function (char) {
+    switch (char) {
+        case " ":
+            return true;
+        case ".":
+            return true;
+        case ",":
+            return true;
+        case "!":
+            return true;
+        case "?":
+            return true;
+        default:
+            return false;
+    }
+};
 
+const palindromes = function (str) {
+    strArray = str
+        .toLowerCase()
+        .split("")
+        .filter((item) => {
+            return !checkChar(item);
+        });
+
+    for (let i = 0; i < strArray.length / 2; i++) {
+        j = strArray.length - i - 1;
+        console.log(`length = ${strArray.length}, i = ${i}, j = ${j}`);
+        if (strArray[i] !== strArray[j]) {
+            console.log("spposed to break");
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
