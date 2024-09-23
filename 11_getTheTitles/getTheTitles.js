68b9d2a351f8c1e0d585f642e34489ed73c6caeb
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-23 15:55:33 +0100

    Exercise 11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titles = [];
+    books.map((book)=>{
+        titles.push(book.title);
+    });
 
+    return titles;
 };
 
 // Do not edit below this line
