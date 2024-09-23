0c3736777c8470703a3cbaa08e537041fcdd8a38
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-23 16:31:46 +0100

    Exercise 12 is complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const calcAge = function (person){
 
+    if( !("yearOfDeath" in person) ){
+        const date = new Date;
+        return date.getFullYear() - person.yearOfBirth;
+    }else{
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
+const findTheOldest = function(people) {
+    let theOldest = people[0];
+    people.forEach(e => {
+        
+        if( calcAge(e) > calcAge(theOldest) ){
+            theOldest = e;
+        }
+
+    });
+    return theOldest;
 };
 
 // Do not edit below this line
