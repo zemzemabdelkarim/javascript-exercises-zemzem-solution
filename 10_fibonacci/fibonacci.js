6957468f2c1ff618156bcd56a42a1db8d8400b1f
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-23 15:48:30 +0100

    Exercise 10 is complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+    let firstNumber = 0;
+    let secondNumber = 1;
+    if(typeof(x) !== 'Number'){
+        x = parseInt(x);
+    }
 
+    if (x < 0) {
+        return "OOPS";
+    } else if (x === 1) {
+        return 1;
+    } else if (x === 2) {
+        return 1;
+    } else if (x === 0) {
+        return 0;
+    }
+
+    
+    let fibo = 2;
+    for (let i = 2; i <= x; i++) {
+        fibo = secondNumber + firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = fibo;
+    }
+    return fibo;
 };
 
 // Do not edit below this line
